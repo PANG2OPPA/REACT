5341ac1201658c9e4301b21651c5f26d9440ffd9
PANG2OPPA <khbkhb85098509@gmail.com>
2023-10-24 14:53:05 +0900

    10월 24일 수업자료입니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Fragment } from "react";
-import "./App.css";
-import Section from "./Section";
-import MyComponent from "./MyComponent";
+import { Fragment } from 'react';
+import './App.css';
+import Section from './Section';
+import MyComponent from './MyComponent';
+import Say from './Say';
 
 function App() {
   return (
     <>
-      <Section
+      {/* <Section
         title="Section 1번"
         content="This is the content for section 1."
         url=""
@@ -15,7 +16,10 @@ function App() {
         title="Section 2번"
         content="This is the content for section 2."
       />
-      <MyComponent name="김현빈" age={24} />
+      <MyComponent name="김현빈" age={24}>
+        안녕하세여~
+      </MyComponent> */}
+      <Say />
     </>
   );
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,14 +1,18 @@
 // props 기본값 설정 : defaultProps
 const MyComponent = (props) => {
+  // 비구조화 할당(구조 분해)  : props 객체의 속성과 값을 변수에 대입 함
+  const { name, age, children } = props;
+
   return (
     <div>
-      안녕하세요. 제 이름은 {props.name}입니다. 나이는 {props.age}살 입니다.
+      안녕하세요. 제 이름은 {name}입니다. 나이는 {p.age}살 입니다.
+      {children}
     </div>
   );
 };
 MyComponent.defaultProps = {
-  name: "기본이름",
-  age: "20",
+  name: '기본이름',
+  age: '20',
 };
 
 export default MyComponent;
